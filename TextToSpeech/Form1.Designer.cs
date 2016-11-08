caa2788e25695a723ab1009e1d2e0fc57a65db8d
Elie <Elie>
2016-11-08 20:28:43 +0200

    first commit

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(13, 13);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(265, 24);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Enter your message below:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(17, 41);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(540, 135);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(17, 183);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(45, 13);
+            this.lblVolume.TabIndex = 2;
+            this.lblVolume.Text = "Volume:";
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.LargeChange = 10;
+            this.trackBarVolume.Location = new System.Drawing.Point(69, 183);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(488, 45);
+            this.trackBarVolume.SmallChange = 5;
+            this.trackBarVolume.TabIndex = 3;
+            this.trackBarVolume.Value = 80;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(17, 245);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(41, 13);
+            this.lblSpeed.TabIndex = 4;
+            this.lblSpeed.Text = "Speed:";
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.LargeChange = 2;
+            this.trackBarSpeed.Location = new System.Drawing.Point(69, 245);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(488, 45);
+            this.trackBarSpeed.TabIndex = 5;
+            this.trackBarSpeed.Value = 1;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(20, 314);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 6;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(116, 314);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(307, 314);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 8;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(482, 313);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Location = new System.Drawing.Point(211, 314);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(75, 23);
+            this.btnContinue.TabIndex = 7;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 354);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.Text = "Text To Speech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnContinue;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer ss;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ss = new SpeechSynthesizer();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            ss.Rate = trackBarSpeed.Value;
+            ss.Volume = trackBarVolume.Value;
+            ss.SpeakAsync(txtMessage.Text);
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            ss.Pause();
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            ss.Resume();
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer ss = new SpeechSynthesizer();
+            ss.Rate = trackBarSpeed.Value;
+            ss.Volume = trackBarVolume.Value;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Wav Files| *.wav";
+            sfd.ShowDialog();
+            ss.SetOutputToWaveFile(sfd.FileName);
+            ss.Speak(txtMessage.Text);
+            ss.SetOutputToDefaultAudioDevice();
+            MessageBox.Show("Recording Completed", "Information");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
